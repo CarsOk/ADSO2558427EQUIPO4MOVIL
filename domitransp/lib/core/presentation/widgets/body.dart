bed872f88d68b4eecc4e3566a22c6e30eb690317
GoJhon <jsantander1219@gmail.com>
2023-09-11 22:53:12 +0000

    bloc home

---
+++ domitransp/lib/core/presentation/widgets/body.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:domitransp/core/presentation/bloc/home_bloc.dart';
+import 'package:domitransp/core/presentation/widgets/home.dart';
+import 'package:domitransp/widgets/loading_animate.dart';
+
+class Body extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocConsumer<HomeBloc, HomeState>(
+        listener: (context, state) {
+
+        },
+        builder: (context, state) {
+          if (state is HomeLoading){
+            return LoadingAnimate();
+          }
+          return Home();
+        },
+      ),
+    );
+  }}
