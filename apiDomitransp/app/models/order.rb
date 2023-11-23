ef88faacca864f910a316abc9a79d989d91b1b90
JuanCito16 <juanvides112011@gmail.com>
2023-11-23 02:17:15 +0000

    Post order y Get users

--- apiDomitransp/app/models/order.rb
+++ apiDomitransp/app/models/order.rb
@@ -1,8 +1,19 @@
 class Order < ApplicationRecord
   has_many :packs, dependent: :destroy
-  # accepts_nested_attributes_for :packs, allow_destroy: true, reject_if: proc { |att| att['tipo'].blank? }
+  accepts_nested_attributes_for :packs, allow_destroy: true, reject_if: proc { |att| att['tipo'].blank? }
   belongs_to :dispatch, optional: true
   belongs_to :user
   belongs_to :company
   attribute :valor, :decimal, default: 0
+  before_save :generate_shipping_code
+  before_create :set_fecha
+  private 
+  
+  def generate_shipping_code
+    self.codigo_envio = SecureRandom.hex(10).upcase
+  end
+
+  def set_fecha
+    self.fecha = Date.current
+  end
 end
